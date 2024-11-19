b45d1b4e35bfa1161451575a755db802be4dbd6b
GuillaumeMendle <guillaume.mendlevitch@gmail.com>
2024-11-19 22:00:24 +0100

    Building pieline

---
+++ project_code/utils.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import os
+import pyvis
+import networkx as nx
+from matplotlib import pyplot as plt
+from itertools import product
+import plotly.graph_objects as go
+import plotly.express as px
+import seaborn as sns
+import project_code.utils as ut
+
+CURRENT_DIRECTORY = os.getcwd()
